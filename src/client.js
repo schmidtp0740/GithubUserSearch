6c286a368fdabaae8cada9f030f69c1e824bd789
schmidtp070 <schmidtp0740@gmail.com>
2017-05-18 19:35:12 -0500

    init project

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Layout from './components/Layout';
+const app = document.getElementById('app');
+ReactDom.render(<Layout/>, app);
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+    state = {  }
+    render() {
+        return (
+            <h1>Layout app</h1>
+        );
+    }
+}
+
+export default Layout;
